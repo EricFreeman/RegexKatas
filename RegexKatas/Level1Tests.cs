1dbc8ed4110e0e1f85a4eadad741270288265d74
Eric Freeman <eric.freeman@extendhealth.com>
2014-01-02 16:51:03 -0700

    initial commit

---
+++ RegexKatas/Level1Tests.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+using Xunit;
+
+namespace RegexKatas
+{
+    public class Level1Tests
+    {
+        public const string TestVar1 = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)";
+        public const string TestVar2 = "Mozilla/4.75 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14";
+        public const string TestVar3 = "Mozilla/NoVersion (compatible; MSIE 6.5; Windows NT 5.5)";
+
+        /// <summary>
+        /// 1) Find mozilla version number without explicitly matching on it (eg don't check for the text '4.75').
+        /// </summary>
+        [Fact]
+        public static void Test1()
+        {
+            const string regex = @"(put answer here)";
+            const int captureGroup = 0; // use to configure which capture group your answer is in
+
+            var m = Regex.Match(TestVar1, regex); 
+            var m2 = Regex.Match(TestVar2, regex);
+            var m3 = Regex.Match(TestVar3, regex);
+
+            Assert.True(m.Success && m.Groups[captureGroup].Value.Equals("4.0"));
+            Assert.True(m2.Success && m2.Groups[captureGroup].Value.Equals("4.75"));
+            Assert.False(m3.Success);
+        }
+
+        /// <summary>
+        /// 1) Find Windows NT version number without explicitly matching on it (eg don't check for the text '6.0').
+        /// </summary>
+        [Fact]
+        public static void Test2()
+        {
+            const string regex = @"(put answer here)";
+            const int captureGroup = 0;
+
+            var m = Regex.Match(TestVar1, regex);
+            var m2 = Regex.Match(TestVar2, regex);
+            var m3 = Regex.Match(TestVar3, regex);
+
+            Assert.True(m.Success && m.Groups[captureGroup].Value.Equals("6.0"));
+            Assert.True(m2.Success && m2.Groups[captureGroup].Value.Equals("5.1"));
+            Assert.True(m3.Success && m3.Groups[captureGroup].Value.Equals("5.5"));
+        }
+    }
+}
---
+++ RegexKatas/Level2Tests.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+using Xunit;
+
+namespace RegexKatas
+{
+    public class Level2Tests
+    {
+        public const string TestVar ="Tech Support: (801)-555-1234";
+        public const string TestVar2 = "Corporate: 1(800) 555 3332";
+        public const string TestVar3 = "Manager- 801.555.2345";
+        public const string TestVar4 = "HR 874-555-3256";
+
+        /// <summary>
+        /// 1) Find all the phone numbers from the four test strings using the same regex.
+        /// </summary>
+        [Fact]
+        public static void Test1()
+        {
+            const string regex = @"(put answer here)";
+            const int captureGroup = 0;
+
+            var m = Regex.Match(TestVar, regex);
+            var m2 = Regex.Match(TestVar2, regex);
+            var m3 = Regex.Match(TestVar3, regex);
+            var m4 = Regex.Match(TestVar4, regex);
+
+            Assert.True(m.Success && m2.Success && m3.Success && m4.Success);
+            Assert.True(m.Groups[captureGroup].Value.Equals("(801)-555-1234"));
+            Assert.True(m2.Groups[captureGroup].Value.Equals("1(800) 555 3332"));
+            Assert.True(m3.Groups[captureGroup].Value.Equals("801.555.2345"));
+            Assert.True(m4.Groups[captureGroup].Value.Equals("874-555-3256"));
+        }
+
+        /// <summary>
+        /// 1) Find the contact name for all numbers with an area code of 801
+        /// </summary>
+        [Fact]
+        public static void Test2()
+        {
+            const string regex = @"(put answer here)";
+            const int captureGroup = 0;
+
+            var m = Regex.Match(TestVar, regex);
+            var m2 = Regex.Match(TestVar2, regex);
+            var m3 = Regex.Match(TestVar3, regex);
+            var m4 = Regex.Match(TestVar4, regex);
+
+            Assert.True(m.Success && !m2.Success && m3.Success && !m4.Success);
+            Assert.True(m.Groups[captureGroup].Value.Equals("Tech Support"));
+            Assert.True(m3.Groups[captureGroup].Value.Equals("Manager"));
+        }
+    }
+}
